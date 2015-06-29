98a025261d54985189468aed00a7419d3c621f0b
AndZii <zykov_wat2008@mail.ru>
2015-06-29 16:03:57 -0400

    hoho

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,14 +2,7 @@ class PageController < ApplicationController
   def index
     @title = "SELL ELECTRONICS NYC | QUEENS | BROOKLYN | MANHATTAN"
     @description = 'Best place to sell your iPhone 6 in NYC. We always pay more!'
-    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','']
-
-    @h1 = 'Sell My iPhone 6'
-    @main_p = '
-You looking for a place that buys iPhones? Here we are, ready to help you. <br>
-
-
-'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','iPad', 'Galaxy', 'Note', 'iMac', 'MacBook', 'LapTop', 'Camera']
   end
 
   def help
