acde1431ae32486e0e446428cdef488d98da2491
AndZii <zykov_wat2008@mail.ru>
2015-07-02 17:42:51 -0400

    howru

--- app/controllers/popular_controller.rb
+++ app/controllers/popular_controller.rb
@@ -112,7 +112,7 @@ We always pay more, we will pay right now and we will pay cash. Do not waste you
 
 
   def sell_long
-    @title = "Best Place To Sell You electronics in Long Island"
+    @title = "Best Place To Sell My electronics in Long Island"
     @description = 'We Are The Best Place To Sell Your Electronics In Long Island'
     @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'New York', 'Buyers', 'iPad','Air','Long', 'Island']
 
@@ -126,7 +126,7 @@ If you have any electronic items to sell, call us right now. We always pay more,
   end
 
   def sell_staten
-    @title = "Best Place To Sell You electronics in Staten Island"
+    @title = "Best Place To Sell My electronics in Staten Island"
     @description = 'We Are The Best Place To Sell Your Electronics In Staten Island'
     @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'New York', 'Buyers', 'iPad','Air','Long', 'Staten']
 
@@ -142,49 +142,258 @@ If you have any electronic items to sell, call us right now. We always pay more,
 
   #second_section
 
-  def pawnshop_that_buy_electronics
+  def pawnshop_that_buys_electronics
+    @title = "We Buy Electronics In NYC | Yop Dollar Paid | PawnShop in NYC"
+    @description = 'We Buy Almost All Electronic Items In New York City Area.'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'New York', 'Buyers', 'iPad','Air','Long', 'Staten','Pawnshop']
+
+    @h1 = 'We Buy Almost All Electronic Items In NYC.'
+    @main_p = "Are You looking to pawn electronics? Why pawn, when You can sell and get more for Your electronic devices. \n
+Way pay more cash on the spot that any other dealer at the worldwide network. So come and get you top dollar right now.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
   def sell_electronics_in_nyc
+    @title = "Sell Electronics In NYC | Cash For Your Electronics"
+    @description = 'Come And Sell Your Electronics For Top Cash!'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'New York', 'Buyers', 'iPad','Air','Long', 'Staten','Pawnshop']
+
+    @h1 = 'Sell Your Electronics In New York City Area.'
+    @main_p = "Selling electronics in NYC was never this easy. \n
+Stop spending your time on auctions and dealing with strange dangerous people from city forums. Come and get your best offer right now.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
-  def new_york_city_electronics_buyers
+  def new_york_city_electronic_buyers
+    @title = "New York City Electronic Buyers | Top Dollar On THe Spot "
+    @description = 'Come And Sell Your Electronics For Top Cash In New York Area!'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'New York', 'Buyers', 'iPad','Air','Long', 'Staten','Pawnshop','Buyers']
+
+    @h1 = 'NYC Electronic Buyers.'
+    @main_p = "We Are NYC Electronic Buyers with two location in Manhattan and Queens. \n
+If you've been looking to sell your old electronics, you have arrived at the right place. Call us right now and get your instant cash on the spot.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
-  def nyc_buyers_of_electronics
+  def nyc_iphone_buyers
+    @title = "NYC iPhone Buyers | Top Cash For Your iPhone."
+    @description = 'We Buy All iPhones In NYC Area.'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'New York', 'Buyers', 'iPad','Air','Long', 'Staten','Pawnshop']
+
+    @h1 = 'Sell My Iphone in NYC'
+    @main_p = "Selling electronics in NYC was never this easy. \n
+Stop spending your time on auctions and dealing with strange dangerous people from city forums. Come and get your best offer right now.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
   def where_to_sell_macbook_in_nyc
+    @title = "Sell MacBook In NYC | Top Cash For Your MacBook."
+    @description = 'We Buy All MacBooks In NYC Area.'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'New York', 'Buyers', 'iPad','Air','Long', 'Staten','Pawnshop','MacBook']
+
+    @h1 = 'Sell My MacBook in NYC'
+    @main_p = "Selling MacBooks in NYC was never this easy. \n
+Stop spending your time on auctions and dealing with strange, dangerous people from city forums. Come and get your instant cash right now.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
   def who_buys_electronics_in_nyc
+    @title = "NYC Electronic Buyers | Top Cash For Your Electronics."
+    @description = 'We Buy Almost All Electronics In NYC Area.'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'New York', 'Buyers', 'iPad','Air','Long', 'Staten','Pawnshop','MacBook']
+
+    @h1 = 'Sell My Electronics in NYC'
+    @main_p = "Selling Electronics in NYC was never this easy. \n
+Stop spending your time on auctions and dealing with strange, dangerous people from city forums. Come and get your instant cash right now.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
   def how_to_sell_electronics_in_new_york_city
+    @title = "How To Sell Electronics In NYC | Top Cash For Your Electronics In NYC Area."
+    @description = 'Want To Know How To Sell Electronics In NYC? We Have Perfect Solution.'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'New York', 'Buyers', 'iPad','Air','Long', 'Staten','Pawnshop','MacBook']
+
+    @h1 = 'How To Sell My Electronics In NYC'
+    @main_p = "You want to know how to sell your electronics in NYC? We have perfect solution for You. \n
+Stop spending your time on auctions and dealing with strange, dangerous people from city forums. Come and get your instant cash right now.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
+  end
+
+  def sell_manhattan
+    @title = "Best Place To Sell My electronics in Manhattan"
+    @description = 'We Are The Best Place To Sell Your Electronics In Manhattan'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'New York', 'Buyers', 'iPad','Air','Long', 'Island','Manhattan']
+
+    @h1 = 'Best Place To Sell Electronics In Manhattan'
+    @main_p = "We are best place where you can sell your electronics in Manhattan. \n
+If you have any electronic items to sell, call us right now. We always pay more, get your instant cash on the spot.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
+  end
+
+  def sell_jersey
+    @title = "Best Place To Sell My electronics in New Jersey"
+    @description = 'We Are The Best Place To Sell Your Electronics In New Jersey'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'New', 'York', 'Buyers', 'iPad','Air','Long', 'Staten']
+
+    @h1 = 'Best Place To Sell Electronics In New Jersey'
+    @main_p = "Looking for a place to sell your electronics in New Jersey? You are on the right page. \n
+If you have any electronic items to sell, call us right now. We always pay more, get your instant cash on the spot.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
   #third_section
 
   def sell_electronics_in_queens
+    @title = "Best Place To Sell My electronics in Queens"
+    @description = 'We Are The Best Place To Sell Your Electronics In Queens'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Queens', 'New', 'York', 'Buyers', 'iPad','Air','Long', 'Staten']
+
+    @h1 = 'Best Place To Sell Electronics In Queens'
+    @main_p = "Looking for a place to sell your electronics in Queens? You are on the right page. \n
+If you have any electronic items to sell, call us right now. We always pay more, get your instant cash on the spot.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
   def cell_phone_repair_in_queens
+    @title = "Best Place To Fix Your Cell Phone In NYC | Queens"
+    @description = 'We Are The Best Place To Fix Your Cell Phone In NYC'
+    @key_words = ['Electronics', 'Fix', 'iPhone', 'Sell','Queens', 'New', 'York', 'Buyers', 'iPad','Air','Long', 'Staten']
+
+    @h1 = 'Best Place To Fix Cell Phones In NYC'
+    @main_p = "Looking for a place to fix your cell phone in Queens? You are on the right page. \n
+Any phone, any problem, we can fix everything. Best prices in the city.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
   def where_to_fix_my_electronics_in_queens
+    @title = "Best place to fix phones and tablets in NYC | Phone Repair NYC"
+    @description = 'Looking for a place to fix your phone in NYC? We are here for You!'
+    @key_words = ['Electronics', 'Fix', 'iPhone', 'Sell','Queens', 'New', 'York', 'Buyers', 'iPad','Air','Long', 'Staten']
+
+    @h1 = 'Best Place To Fix Phones and Tablets In NYC'
+    @main_p = "Looking for a place to fix your cell phone in NYC? You are on the right page. \n
+Any phones, any problems, we can fix everything. Best prices in the city.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
   def iphone_repair_in_queens
+    @title = "Best place to fix your iPhone or iPad | Phone Repair NYC"
+    @description = 'Looking for a place to fix your iphone in NYC? We are here for You!'
+    @key_words = ['Electronics', 'Fix', 'iPhone', 'Sell','Queens', 'New', 'York', 'Buyers', 'iPad','Air','Long', 'Staten']
+
+    @h1 = 'Best Place To Fix iPhones and iPads In Queens'
+    @main_p = "Looking for a place to fix your iPhone or iPad in Queens? You are on the right page. \n
+Any device, any problems, we can fix everything. Best prices in the city.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
   def fix_samsung_in_queens
+    @title = "Best place to fix your Samsung Galaxy, Note or Tab | Phone Repair NYC"
+    @description = 'Looking for a place to fix your Samsung phones and tablets in NYC? We are here for You!'
+    @key_words = ['Electronics', 'Fix', 'iPhone', 'Sell','Queens', 'New', 'York', 'Buyers', 'iPad','Air','Long', 'Staten']
+
+    @h1 = 'Best Place To Fix Samsung Galaxy, Note or Tab In Queens'
+    @main_p = "Looking for a place to fix your Samsung in Queens? You are on the right page. \n
+Any phones, any problems, we can fix everything. Best prices in the city.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
   def sell_my_iphone_in_queens
+    @title = "Best Place To Sell My Iphone in Queens"
+    @description = 'We Are The Best Place To Sell Your iPhone In Queens'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Queens', 'New', 'York', 'Buyers', 'iPad','Air','Long', 'Staten']
+
+    @h1 = 'Best Place To Sell iPhone In Queens'
+    @main_p = "Looking for a place to sell your iPhone in Queens? You are on the right page. \n
+If you have any electronic items to sell, call us right now. We always pay more, get your instant cash on the spot.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
   def sell_my_ipad_in_queens
+    @title = "Best Place To Sell My iPad in Queens"
+    @description = 'We Are The Best Place To Sell Your iPad In Queens'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Queens', 'New', 'York', 'Buyers', 'iPad','Air','Long', 'Staten']
+
+    @h1 = 'Best Place To Sell iPad In Queens'
+    @main_p = "Looking for a place to sell your iPad in Queens? You are on the right page. \n
+If you have any electronic items to sell, call us right now. We always pay more, get your instant cash on the spot.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
   end
 
+  def sell_connecticut
+    @title = "Best Place To Sell My electronics in Connecticut"
+    @description = 'We Are The Best Place To Sell Your Electronics In Connecticut'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'Connecticut', 'York', 'Buyers', 'iPad','Air','Long', 'Staten']
+
+    @h1 = 'Best Place To Sell Electronics In Connecticut'
+    @main_p = "Looking for a place to sell your electronics in Connecticut? You are on the right page. \n
+If you have any electronic items to sell, call us right now. We always pay more, get your instant cash on the spot.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
+  end
+
+  def sell_brooklyn
+    @title = "Best Place To Sell My electronics in Brooklyn"
+    @description = 'We Are The Best Place To Sell Your Electronics In Brooklyn'
+    @key_words = ['Electronics', 'NYC', 'iPhone', 'Sell','Plus', 'Brooklyn', 'York', 'Buyers', 'iPad','Air','Long', 'Staten']
+
+    @h1 = 'Best Place To Sell Electronics In Brooklyn'
+    @main_p = "Looking for a place to sell your electronics in Brooklyn? You are on the right page. \n
+If you have any electronic items to sell, call us right now. We always pay more, get your instant cash on the spot.\n
+    "
+    @second_p = "Our queens location open 7 days a week, just walk in. If you want to visit our office in Manhattan, you need to
+ to schedule an appointment. You can call us or use online form."
+    render 'template'
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,49 @@ Rails.application.routes.draw do
   get 'sell-my-electronics-in-staten-island' => 'popular#sell_staten', as: :sell_staten
 
   get 'sell-my-electronics-in-long-island' => 'popular#sell_long', as: :sell_long
+
+
+
+
+  #secont_section
+
+  get 'pawnshop-that-buys-electronics-in-nyc' => 'popular#pawnshop_that_buys_electronics', :as => :pownshops_that_buy_electronics
+
+  get 'sell-electronics-in-nyc' => 'popular#sell_electronics_in_nyc', :as => :sell_electronics_in_nyc
+
+  get 'new-york-city-electronic-buyers' => 'popular#new_york_city_electronic_buyers', :as => :new_york_city_buyers
+
+  get 'new-york-city-iphone-buyers' => 'popular#nyc_iphone_buyers', :as => :nyc_iphone_buyers
+
+  get 'where-to-sell-macbook-in-nyc' => 'popular#where_to_sell_macbook_in_nyc', :as => :where_to_sell_macbook_in_nyc
+
+  get 'who-buys-electronics-nyc' => 'popular#who_buys_electronics_in_nyc', :as => :who_buys_electronics_in_nyc
+
+  get 'how-to-sell-electronics-in-nyc' => 'popular#how_to_sell_electronics_in_new_york_city', :as => :how_to_sell_electronics_in_new_york_city
+
+  get 'sell-electronics-in-Manhattan' => 'popular#sell_manhattan', :as => :sell_manhattan
+
+  get 'sell-electronics-in-New-Jersey' => 'popular#sell_jersey', :as => :sell_jersey
+
+  #third_section
+
+  get 'sell-my-electronics-in-Connecticut' => 'popular#sell_connecticut', :as => :sell_connecticut
+
+  get 'sell-my-electronics-in-Brooklyn' => 'popular#sell_brooklyn', :as => :sell_brooklyn
+
+  get 'sell-my-electronics-in-queens' => 'popular#sell_electronics_in_queens', :as => :sell_queens
+
+  get 'cell-phone-repair-in-queens' => 'popular#cell_phone_repair_in_queens', :as => :cell_phone_repair
+
+  get 'sell-my-iphone-in-queens' => 'popular#sell_my_iphone_in_queens', :as => :iphone_in_queens
+
+  get 'sell-my-ipad-in-queens' => 'popular#sell_my_ipad_in_queens', :as => :ipad_in_queens
+
+  get 'where-to-fix-my-phone-in-nyc' => 'popular#where_to_fix_my_electronics_in_queens', :as => :cell_phone_repair_where
+
+  get 'where-to-fix-my-iphone-in-queens' => 'popular#iphone_repair_in_queens', :as => :cell_phone_repair_iphone
+
+  get 'where-to-fix-my-samsung-in-queens' => 'popular#fix_samsung_in_queens', :as => :cell_phone_repair_samsung
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
